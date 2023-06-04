158cb2c130a6f55eae0398a1672ed975a44f52e6
lcspaiva <lcspaiva87@gmail.com>
2023-06-03 21:52:08 -0300

    fix:creating templates and creating a route for editing user data

--- src/controller/user/userController.ts
+++ src/controller/user/userController.ts
@@ -1,9 +1,8 @@
-// controllers/userController.ts
-
+/* eslint-disable no-undef */
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { PrismaClient } from '@prisma/client'
 import { UserAdminSchema } from '../../models/userAdmin'
-
+import { z } from 'zod'
 const prisma = new PrismaClient()
 
 async function registerUser(request: FastifyRequest, reply: FastifyReply) {
@@ -33,5 +32,46 @@ async function registerUser(request: FastifyRequest, reply: FastifyReply) {
     reply.status(400).send({ message: error })
   }
 }
+async function editUser(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const paramsSchema = z.object({
+      id: z.string(),
+    })
+    const { id } = paramsSchema.parse(request.params)
 
-export { registerUser }
+    const { name, email, password, quantityTables } = UserAdminSchema.parse(
+      request.body,
+    )
+
+    // Verificar se o email já está sendo usado por outro usuário
+    const existingUser = await prisma.userAdmin.findFirst({
+      where: {
+        email,
+        NOT: {
+          id,
+        },
+      },
+    })
+    if (existingUser) {
+      return reply.status(400).send({ message: 'Email already exists' })
+    }
+
+    const updatedUser = await prisma.userAdmin.update({
+      where: { id },
+      data: {
+        name,
+        email,
+        password,
+        quantityTables,
+      },
+    })
+
+    reply
+      .status(200)
+      .send({ message: 'User updated successfully', user: updatedUser })
+  } catch (error) {
+    console.error(error)
+    reply.status(400).send({ message: error })
+  }
+}
+export { registerUser, editUser }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 // routes/userRoutes.ts
 
 import { FastifyInstance } from 'fastify'
-import { registerUser } from '../controller/user/userController'
+import { editUser, registerUser } from '../controller/user/userController'
 
 async function userRoutes(fastify: FastifyInstance) {
   fastify.post('/register', registerUser)
+  fastify.put('/users/:id', editUser)
 }
 
 export default userRoutes
